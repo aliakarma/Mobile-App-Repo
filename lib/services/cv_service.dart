4f7f12720ad888ce04f7b624aabd948663d0d6c7
aliakarma <443059463@stu.iu.edu.sa>
2026-04-20 00:49:55 +0300

    Bump default API port to 8001
    Update CvService.defaultBaseUrl to use port 8001 instead of 8000 across platforms (web, Android emulator and other target platforms). This aligns the client with the backend now running on port 8001.

--- lib/services/cv_service.dart
+++ lib/services/cv_service.dart
@@ -12,17 +12,17 @@ class CvService {
 
   static String get defaultBaseUrl {
     if (kIsWeb) {
-      return 'http://localhost:8000';
+      return 'http://localhost:8001';
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        return 'http://10.0.2.2:8000';
+        return 'http://10.0.2.2:8001';
       case TargetPlatform.iOS:
       case TargetPlatform.macOS:
       case TargetPlatform.windows:
       case TargetPlatform.linux:
       case TargetPlatform.fuchsia:
-        return 'http://localhost:8000';
+        return 'http://localhost:8001';
     }
   }
 
